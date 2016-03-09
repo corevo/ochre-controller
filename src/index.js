518846233ebbb0b56ffb2f5a9a17835d9827b511
Tomer <gmtomer@gmail.com>
2016-03-09 11:53:00 +0200

    dont copy code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import index from 'ochre-indexer';
 import kue from 'kue';
+import find from 'find';
 
+const FILES_PATH = process.env.FILES_PATH || '/data';
 const redis = {
-    port: process.env.REDIS_PORT | 6379,
-    host: process.env.REDIS_HOST | '127.0.0.1'
+    port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || '127.0.0.1'
 };
 const queue = kue.createQueue({
     redis
 });
 
-queue.create().removeOnComplete(true).save();
+find.file(/^((?!\/\.).)*$/, FILES_PATH, (files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+    //queue.create().removeOnComplete(true).save();
+});

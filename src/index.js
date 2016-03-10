e606844547ed22fa0cabb7d55f0bc284fc97975a
Tomer <gmtomer@gmail.com>
2016-03-10 11:09:28 +0200

    oo refactor

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import kue from 'kue';
 import find from 'find';
 
-const FILES_PATH = process.env.FILES_PATH || '/data';
-const redis = {
-    port: process.env.REDIS_PORT || 6379,
-    host: process.env.REDIS_HOST || '127.0.0.1'
-};
-const queue = kue.createQueue({
-    redis
-});
-
 function getFiles(path, cb) {
     find.file(path, (files) => {
         cb(files.filter(file => (!(/(^|\/)\.[^\/\.]/g).test(file))));
     });
 }
 
-getFiles(FILES_PATH, files => {
-    files.forEach(file => {
-        console.log('Creating job for: ' + file);
-        queue.create('index', {
-            file
-        }).removeOnComplete(true).save();
-    });
-});
+export default class Controller {
+    constructor(host, port, path) {
+        super();
+        const redis = {
+            port: process.env.REDIS_PORT || 6379,
+            host: process.env.REDIS_HOST || '127.0.0.1'
+        };
+        this.queue = kue.createQueue({
+            redis
+        });
+    }
+    index() {
+        getFiles(FILES_PATH, files => {
+            files.forEach(file => {
+                console.log('Creating job for: ' + file);
+                queue.create('index', {
+                    file
+                }).removeOnComplete(true).save();
+            });
+        });
+    }
+}

6af71ab164099f06e3b4a7769b545df756683999
Tomer <gmtomer@gmail.com>
2016-03-10 11:01:52 +0200

    works for files path too

--- src/cli.js
+++ src/cli.js
@@ -7,21 +7,26 @@ process.title = "ochre commander";
 
 program
     .version(require('../package.json').version)
-    .usage('[options] <path to index>')
+    .usage('[options] <absolute path to index>')
     .option('-a, --address', 'Redis address, if not specified will use REDIS_HOST or localhost')
     .option('-p, --port', 'Redis port, if not specified will use REDIS_PORT or 6379')
     .parse(process.argv);
 
 let port = program.port;
-if (port === undefined) {
+if (!port) {
     port = process.env.REDIS_PORT || 6379;
 }
 
 let address = program.address;
-if (address === undefined) {
+if (!address) {
     address = process.env.REDIS_HOST || '127.0.0.1';
 }
 
-console.log(program.address);
-console.log(program.port);
-console.log(program.args);
+let path = program.args[0];
+if (!path) {
+    path = process.env.FILES_PATH || '/data';
+}
+
+console.log(address);
+console.log(port);
+console.log(path);
